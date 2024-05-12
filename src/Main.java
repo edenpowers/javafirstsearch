afdd989431d6e3f33ac32a9345a0fbc314f93bbe
edenpowers <eden.powers00@gmail.com>
2024-05-12 19:48:28 -0400

    added graph test to Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 package src;
 import java.util.ArrayList;
-
+import java.util.Optional;
+import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
@@ -20,11 +21,21 @@ public class Main {
         e.setChildren(new Node[]{a,f});
         f.setChildren(new Node[]{b,e});
         g.setChildren(new Node[]{c});
-
+        
         Search search = new Search();
         ArrayList<Node> solution = search.runSearch(a, d);
         for(Node curNode:solution){
             System.out.println(curNode.getID());
+        }
+
+        Graph graph = new Graph(new Node[]{a,b,c,d,e,f,g});
+        Optional<Stack<Node>> path = graph.search(f, e);
+        if(path.isPresent()){
+            for(Node curNode : path.get()){
+                System.out.println(curNode.getID());
+            }
+        } else{
+            System.out.println("search failed");
         }
     }   
 }
