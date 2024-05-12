d71fb8b8d32a7940eac8e636c4f36dc903322d37
edenpowers <eden.powers00@gmail.com>
2024-05-12 19:42:49 -0400

    remembered to add Graph class, rewrote test in Main

---
+++ src/Graph.java
@@ -0,0 +1,49 @@
+package src;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Stack;
+public class Graph {
+    Node[] nodes;
+    public Graph(){
+        nodes = new Node[0];
+    }
+    public Graph(Node[] nodes){
+        this.nodes = nodes;
+    }
+    public Optional<Stack<Node>> search(Node current, Node target){
+        return search(current, target, new ArrayList<Node>());
+    }
+    private Optional<Stack<Node>> search(
+        Node current, Node target, ArrayList<Node> discovered
+    ){
+        if(current.equals(target)){
+            Stack<Node> temp = new Stack<Node>();
+            temp.push(current);
+            return Optional.of(temp);
+        }
+        discovered.add(current);
+        for(Node x : current.getChildren()){
+            boolean alr_checked = false;
+            for(Node y: discovered){
+                if(y.equals(x)){
+                    alr_checked = true;
+                }
+            }
+            if(!alr_checked){
+                Optional<Stack<Node>> temp = search(x,target,discovered);
+                if(temp.isPresent()){
+                    temp.get().push(current);
+                    return temp;
+                }
+            }
+        }
+        return Optional.empty();
+    }
+    public String toString(){
+        String str = "Graph with nodes: ";
+        for(Node x : nodes){
+            str += x.getID() + ", ";
+        }
+        return str;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
         //Test case using the graph here https://en.wikipedia.org/wiki/File:Graph.traversal.example.png
-        //Note this program currently only supports trees, and doesn't support weights
-        Node f = new Node("f");
-        ArrayList<Node> x = new ArrayList<Node>();
-        x.add(f);
-        Node e = new Node("e",x);
-        Node d = new Node("d");
-        ArrayList<Node> z = new ArrayList<Node>();
-        z.add(f);
-        z.add(d);
-        Node b = new Node("b",z);
-        Node g = new Node("g");
-        ArrayList<Node> y = new ArrayList<Node>();
-        y.add(g);
-        Node c = new Node( "c",y);
-        ArrayList<Node> w = new ArrayList<Node>();
-        w.add(b);
-        w.add(c);
-        w.add(e);
-        Node a = new Node("a", w);
+        //Note this program currently doesn't support weights
+        Node a = new Node("a", 1);
+        Node b = new Node("b", 1);
+        Node c = new Node("c", 1);
+        Node d = new Node("d", 1);
+        Node e = new Node("e", 1);
+        Node f = new Node("f", 1);
+        Node g = new Node("g", 1);
+        a.setChildren(new Node[]{b,c,e});
+        b.setChildren(new Node[]{a,d,f});
+        c.setChildren(new Node[]{a,g});
+        d.setChildren(new Node[]{b});
+        e.setChildren(new Node[]{a,f});
+        f.setChildren(new Node[]{b,e});
+        g.setChildren(new Node[]{c});
+
         Search search = new Search();
         ArrayList<Node> solution = search.runSearch(a, d);
         for(Node curNode:solution){
