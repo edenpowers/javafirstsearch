329f5fa63ee369709899c7b5e74612665d14b530
edenpowers <eden.powers00@gmail.com>
2024-05-12 20:16:36 -0400

    removed weights from main

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         //Test case using the graph here https://en.wikipedia.org/wiki/File:Graph.traversal.example.png
         //Note this program currently doesn't support weights
-        Node a = new Node("a", 1);
-        Node b = new Node("b", 1);
-        Node c = new Node("c", 1);
-        Node d = new Node("d", 1);
-        Node e = new Node("e", 1);
-        Node f = new Node("f", 1);
-        Node g = new Node("g", 1);
+        Node a = new Node("a");
+        Node b = new Node("b");
+        Node c = new Node("c");
+        Node d = new Node("d");
+        Node e = new Node("e");
+        Node f = new Node("f");
+        Node g = new Node("g");
         a.setChildren(new Node[]{b,c,e});
         b.setChildren(new Node[]{a,d,f});
         c.setChildren(new Node[]{a,g});
